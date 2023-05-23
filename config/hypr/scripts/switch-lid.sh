6eed0e63636dfaa31be753100037ac39b9a622a5
Abhishek Meena <58336567+Aacle@users.noreply.github.com>
2023-05-24 02:43:41 +0530

    Update switch-lid.sh

--- config/hypr/scripts/switch-lid.sh
+++ config/hypr/scripts/switch-lid.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if grep open /proc/acpi/button/lid/LID/state; then
-    hyprctl keyword monitor "eDP-1, 2560x1440@165, 0x0, 1"
+if [ grep open /proc/acpi/button/lid/LID/state ] | [ grep open /proc/acpi/button/lid/LID0/state ]; then
+    hyprctl keyword monitor "eDP-1, preferred, auto, 1"
 else
     if [[ `hyprctl monitors | grep "Monitor" | wc -l` != 1 ]]; then
         hyprctl keyword monitor "eDP-1, disable"
